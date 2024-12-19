77ec96efe2fcc81e1ea794d89adc31f93de9ed9a
sushil thakur <sushilt2059@gmail.com>
2024-12-19 18:34:46 +0545

    push

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
-      <div>hello my first class based Component</div>
+      <div>
+        <NavBar />
+        <News />
+      </div>
     )
   }
 }
---
+++ src/components/News.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>News
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+      </div>
+    )
+  }
+}
+
+export default News
