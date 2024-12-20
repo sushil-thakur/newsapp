ebda55e3ea19f829f2939e3f68bdb8964bda8afb
sushil thakur <sushilt2059@gmail.com>
2024-12-20 12:27:41 +0545

    push

--- src/components/News.js
+++ src/components/News.js
@@ -4,16 +4,22 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>News
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className='container my-3'>
+        <h2>Top Headlines</h2>
+       <div className="row">
+                  <div className="col-md-4">
+                  <NewsItem  title="mytitlr" description ="mydescription"/>
+                      <NewsItem  title="mytitlr" description ="mydescription"/>
+                  <div className="col-md-4">
+                  <NewsItem  title="mytitlr" description ="mydescription"/>
+                  </div>
+              </div>
+      
+        </div>
       </div>
     )
   }
 }
 
 export default News
+// 0f895488251f4fffa70a558d272114bf
