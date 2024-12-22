5e6bd6e96ab65c3037817b147ec745495144ec71
sushil thakur <sushilt2059@gmail.com>
2024-12-22 19:21:47 +0545

    push

--- src/components/News.js
+++ src/components/News.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
 
 export class News extends Component {
+  constructor() {
+    super();
+  }
   render() {
     return (
-      <div className='container my-3'>
+      <div className="container my-3">
         <h2>Top Headlines</h2>
-       <div className="row">
-                  <div className="col-md-4">
-                  <NewsItem  title="mytitlr" description ="mydescription" imgUrl="https://www.shutterstock.com/search/breaking-news-colour"/>
-                      <NewsItem  title="mytitlr" description ="mydescription"/>
-                  <div className="col-md-4">
-                  <NewsItem  title="mytitlr" description ="mydescription"/>
-                  </div>
-              </div>
-      
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem 
+              title="My Title" 
+              description="My Description" 
+              imgUrl="https://www.pexels.com/photo/camping-among-trees-with-city-behind-at-night-15838266/" 
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem 
+              title="My Title 2" 
+              description="My Description 2" 
+              url="https://www.example.com" 
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem 
+              title="My Title 3" 
+              description="My Description 3" 
+              url="https://www.anotherexample.com" 
+            />
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
 // 0f895488251f4fffa70a558d272114bf
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  constructor() {
+    super();
+  }
   render() {
    let {title, description, imgUrl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
                   <img src={imgUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
                           <h5 className="card-title">{title}</h5>
                           <p className="card-text">{description}</p>
-                          <a href="#" className="btn btn-primary">Go somewhere</a>
+                          <a href="#" className="btn btn-sm btn btn-primary">Read more</a>
                         </div>
                 </div>
       </div>
